79037f8f3681c67e938270c028fe07c145a1a0bc
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-18 16:22:43 -0400

    while loops

--- main.dart
+++ main.dart
@@ -1,9 +1,12 @@
 int a = 1;
-int b = 5;
+int b = 0;
 
 void main() {
   for (int i = 0; i < 10000000; i += 1) {
     print("i = $i ");
+    if (b < 5) {
+      b += 1;
+    }
     while (b == 5) {
       print("b = $b ");
       if (i == 50000) {
