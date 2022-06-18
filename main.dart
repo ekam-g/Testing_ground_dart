135911188e363ef7dafe86e17e2b32e7fbcfb29f
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-18 19:02:00 -0400

    idk

--- main.dart
+++ main.dart
@@ -1,22 +1,25 @@
+import 'testingcross.dart';
+
 int a = 1;
 int b = 0;
 
 void main() {
   code();
-  demo(1,2);
+  addingDemo();
+  p("done");
 }
 
 class code {
   code() {
     for (int i = 0; i < 10000000; i += 1) {
-      print("i = $i ");
+      p("i = $i ");
       if (b < 5) {
         b += 1;
       }
       while (b == 5) {
-        print("b = $b ");
+        p("b = $b ");
         if (i == 50000) {
-          print("worked");
+          p("worked");
           return;
         }
         break;
@@ -25,9 +28,12 @@ class code {
   }
 }
 
-class demo {
-  demo(int t, int ti) {
-    print("find");
-    print("a = $t b = $ti");
+
+class addingDemo {
+  int a = 1;
+  int b = 3;
+  addingDemo() {
+   int c = add(a, b);
+    p("$c");
   }
 }
---
+++ testingcross.dart
@@ -0,0 +1,14 @@
+p(String p) {
+  bool debug = true;
+
+  if (debug == true) {
+    print(p);
+  } else {
+    return;
+  }
+}
+
+add(int a, int b) {
+  b += a;
+  return b;
+}
