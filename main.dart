ffa50e8f950db8383df11fc2e2035f8ce894b766
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-18 16:44:23 -0400

    func

--- main.dart
+++ main.dart
@@ -2,18 +2,32 @@ int a = 1;
 int b = 0;
 
 void main() {
-  for (int i = 0; i < 10000000; i += 1) {
-    print("i = $i ");
-    if (b < 5) {
-      b += 1;
-    }
-    while (b == 5) {
-      print("b = $b ");
-      if (i == 50000) {
-        print("worked");
-        return;
+  code();
+  demo(1,2);
+}
+
+class code {
+  code() {
+    for (int i = 0; i < 10000000; i += 1) {
+      print("i = $i ");
+      if (b < 5) {
+        b += 1;
       }
-      break;
+      while (b == 5) {
+        print("b = $b ");
+        if (i == 50000) {
+          print("worked");
+          return;
+        }
+        break;
+      }
     }
+  }
+}
+
+class demo {
+  demo(int t, int ti) {
+    print("find");
+    print("a = $t b = $ti");
   }
 }
