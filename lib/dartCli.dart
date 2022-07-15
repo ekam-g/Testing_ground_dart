086cb085344b9d58fbd464805000cde7c147bdf6
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-14 20:09:16 -0400

    learned switch case

--- lib/dartCli.dart
+++ lib/dartCli.dart
@@ -1,36 +1,37 @@
 import 'dart:io';
 
 cli() {
-
   dynamic input;
-  List numToAdd = [0.0,0.0];
-
+  List numToAdd = [0.0, 0.0];
 
   print('Welcome to Dart CLI');
   while (true) {
     input = stdin.readLineSync();
-    if (input == 'exit') {
-      return 0;
-    }
-    else if (input == 'add') {
-      print('Enter two numbers to add');
-      for (int x = 0; x < 2; x++) {
-        try {
-          numToAdd[x] = double.parse(stdin.readLineSync());
-        } catch (e) {
-          print(e);
-          x--;
+    switch (input) {
+      case 'exit':
+        return 0;
+        break;
+
+      case 'add':
+        print('Enter two numbers to add');
+        for (int x = 0; x < 2; x++) {
+          try {
+            numToAdd[x] = double.parse(stdin.readLineSync());
+          } catch (e) {
+            print(e);
+            x--;
+          }
         }
-      }
-      numToAdd[0] += numToAdd[1];
-      print(numToAdd[0]);
-    }
-    else if (input == 'help') {
-      print('add - adds two numbers\n'
-          'exit - exits the program');
-    }
-    else {
-      print('Invalid input');
+        numToAdd[0] += numToAdd[1];
+        print(numToAdd[0]);
+        break;
+
+      default:
+        print('Invalid input\n'
+            'here are the valid commands:\n'
+            'add - adds two numbers\n'
+            'exit - exits the program');
+        break;
     }
   }
 }
